804b4bcc551573ef65235f2007e44de44d217cdb
N1Dovud <skaiqhcvab72@bk.ru>
2024-08-10 01:22:08 +0500

    feature: Added a functionality to sent an email

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-
+  document.querySelector('#compose-form').addEventListener("submit", submit_email);
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +30,33 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+// Send Mail: When a user submits the email composition form, add JavaScript code to actually send the email.
+// You’ll likely want to make a POST request to /emails, passing in values for recipients, subject, and body.
+// Once the email has been sent, load the user’s sent mailbox.
+async function submit_email(e) {
+  e.preventDefault();
+  let recipients = document.querySelector("#compose-recipients").value;
+  let subject = document.querySelector("#compose-subject").value;
+  const body = document.querySelector("#compose-body").value;
+  const email = {
+    recipients: recipients,
+    subject: subject,
+    body: body,
+  }
+  try {
+    const response = await fetch("/emails", {
+      method: "POST",
+      body: JSON.stringify(email) });
+    const result = await response.json();
+    if (result.message != undefined) {
+      alert(result.message);
+    } else {
+      alert(result.error);
+    }
+    load_mailbox('sent');
+  }
+  catch(err) {
+    alert("Error:", err.message);
+  }
 }
