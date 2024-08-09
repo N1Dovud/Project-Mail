a75c839e80bcd3756c4ab54be1dfefc12da5250a
N1Dovud <skaiqhcvab72@bk.ru>
2024-08-10 01:25:08 +0500

    fix: Added comments to submit_email function

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+
+  //Attaching event handler when submitting email form
   document.querySelector('#compose-form').addEventListener("submit", submit_email);
   // By default, load the inbox
   load_mailbox('inbox');
@@ -31,24 +33,33 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
-// Send Mail: When a user submits the email composition form, add JavaScript code to actually send the email.
-// You’ll likely want to make a POST request to /emails, passing in values for recipients, subject, and body.
-// Once the email has been sent, load the user’s sent mailbox.
+
+// function for submitting an email
 async function submit_email(e) {
+
+  // prevent default behaviour
   e.preventDefault();
+
+  // get the values
   let recipients = document.querySelector("#compose-recipients").value;
   let subject = document.querySelector("#compose-subject").value;
   const body = document.querySelector("#compose-body").value;
+
+  // create the needed object
   const email = {
     recipients: recipients,
     subject: subject,
     body: body,
   }
+
   try {
+    // do the fetching
     const response = await fetch("/emails", {
       method: "POST",
       body: JSON.stringify(email) });
     const result = await response.json();
+
+    //tell the user whether everything worked fine
     if (result.message != undefined) {
       alert(result.message);
     } else {
@@ -56,6 +67,7 @@ async function submit_email(e) {
     }
     load_mailbox('sent');
   }
+  //in case something goes wrong
   catch(err) {
     alert("Error:", err.message);
   }
